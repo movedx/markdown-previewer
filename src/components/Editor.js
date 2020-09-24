d0a586564cdf50c7d7f528f3be70df6dd83f8ad8
movedx <grytsenko.bogdan@gmail.com>
2020-09-24 21:40:16 +0200

    add main components

---
+++ src/components/Editor.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Editor = () => {
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        border: "1px solid black",
+      }}
+    >
+      Editor
+    </div>
+  );
+};
+
+export default Editor;
---
+++ src/components/Previewer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Previewer = () => {
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        border: "1px solid black",
+      }}
+    >
+      Previewer
+    </div>
+  );
+};
+
+export default Previewer;
